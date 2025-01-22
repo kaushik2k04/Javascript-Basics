84b5299cb2f5a35ffa40a1ada445ffd8e42c31bd
KAUSHIK A <kskaushik031@gmail.com>
2025-01-22 12:20:27 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function add(){
+    var num=document.getElementById('num1');
+    let num1=num.value;
+    var numm=document.getElementById('num2');
+    let num2=numm.value;
+    let a=parseInt(num1)+parseInt(num2);
+    console.log(a);
+}
+function sub(){
+    var num=document.getElementById('num1');
+    let num1=num.value;
+    var numm=document.getElementById('num2');
+    let num2=numm.value;
+    let a=parseInt(num1)-parseInt(num2);
+    console.log(a);
+}
+function mul(){
+    var num=document.getElementById('num1');
+    let num1=num.value;
+    var numm=document.getElementById('num2');
+    let num2=numm.value;
+    let a=parseInt(num1)+parseInt(num2);
+    console.log(a);
+}
+function mul(){
+    var num=document.getElementById('num1');
+    let num1=num.value;
+    var numm=document.getElementById('num2');
+    let num2=numm.value;
+    let a=parseInt(num1)/parseInt(num2);
+    console.log(a);
+}
