1d5d70d1394a0b1441afbc92bdc438ba98b35051
KAUSHIK A <kskaushik031@gmail.com>
2025-01-22 12:19:07 +0530

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,12 @@
+alert("hello world");
+function print(){
+   
+    var name=document.getElementById('name');
+    let na=name.value;
+
+    var age=document.getElementById('age');
+    let ag=age.value;
+
+    alert("hello "+na+" your age is "+ag);
+
+}
